7865b9c07f31b8548c451a28c202a166b0687a46
AboodXD <aboood40091@gmail.com>
2016-08-27 22:40:01 +0400

    Version 1.0

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wii U BFLIM Extractor
+Extracts textures from the FLIM format used in Wii U games.  
+  
+Can Also convert .png files into .bflim files!  
+
+# Special thanks to:
+Exzap - Helping with swizzling.
---
+++ build.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# BFLIM Extractor
+# Version v1.0
+# Copyright © 2016 AboodXD
+
+# This file is part of BFLIM Extractor.
+
+# BFLIM Extractor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# BFLIM Extractor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""build.py: Build an executable for BFLIM Extractor."""
+
+import os, shutil, sys
+from cx_Freeze import setup, Executable
+
+version = 'v1.0'
+
+# Pick a build directory
+dir_ = 'bflim_extract ' + version
+
+# Add the "build" parameter to the system argument list
+if 'build' not in sys.argv:
+    sys.argv.append('build')
+
+# Clear the directory
+print('>> Clearing/creating directory...')
+if os.path.isdir(dir_): shutil.rmtree(dir_)
+os.makedirs(dir_)
+print('>> Directory ready!')
+
+# exclude QtWebKit to save space, plus Python stuff we don't use
+excludes = ['doctest', 'pdb', 'unittest', 'difflib', 'inspect',
+    'os2emxpath', 'posixpath', 'optpath', 'locale', 'calendar',
+    'select', 'multiprocessing', 'ssl',
+    'PyQt5.QtWebKit', 'PyQt5.QtNetwork']
+
+setup(
+    name = 'bflim_extract',
+    version = version,
+    description = 'Wii U BFLIM decoder',
+    options={
+        'build_exe': {
+            'excludes': excludes,
+            'build_exe': dir_,
+            },
+        },
+    executables = [
+        Executable(
+            'bflim_extract.py',
+            ),
+        ],
+    )
+
+print('>> Attempting to copy required files...')
+shutil.copy('COPYING', dir_)
+shutil.copy('nvdxt.exe', dir_)
+shutil.copy('README.md', dir_)
+print('>> Files copied!')
+
+print('>> BFLIM Extractor has been frozen to %s !' % dir_)
